59775bc815e95f7e71a03edea74c9a363eb1bab6
aleksmn <al-mn@yandex.ru>
2024-01-07 13:40:56 +0300

    make axios request

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,12 @@ const app = express();
 app.use(express.json());
 
 // CORS - Cross Origin Resourse Sharing
+// app.use(
+//     cors()
+// );
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'http://localhost:5173',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type']
     })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import axios from 'axios';
+
 // sfc
 const App = () => {
+
+  useEffect(() => {
+    axios.get("http://localhost:5555/books").then(
+      response => console.log(response)
+    )
+  })
+
+
   return ( 
-    <div className="container">
+    <div className="container p-4">
       <h1>Всем привет!</h1>
     </div>
    );
